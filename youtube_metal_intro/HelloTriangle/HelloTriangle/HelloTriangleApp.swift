a64c131295649040cf5e350a81b4737694e5aba1
ethan <140538808+e-d-p-9@users.noreply.github.com>
2024-03-21 00:54:55 -0500

    add chapter 1 hello triangle project

---
+++ youtube_metal_intro/HelloTriangle/HelloTriangle/HelloTriangleApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloTriangleApp.swift
+//  HelloTriangle
+//
+//  Created by Ethan Peacock on 3/20/24.
+//
+
+import SwiftUI
+
+@main
+struct HelloTriangleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
