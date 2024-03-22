9368bfd504c713afe4b5e979083a2110a7cc7ab3
ethan <140538808+e-d-p-9@users.noreply.github.com>
2024-03-22 14:26:51 -0500

    Add directory and beginner project for objective c big nerd ranch book

---
+++ ObjectiveC_BNRG/AGoodStart/AGoodStart/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  AGoodStart
+//
+//  Created by Ethan Peacock on 3/22/24.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+    // insert code here...
+
+}
