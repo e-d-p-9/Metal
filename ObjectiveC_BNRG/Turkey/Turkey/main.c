e8a7bdd15303dd52930d60d85e00b40fbe9c031a
ethan <140538808+e-d-p-9@users.noreply.github.com>
2024-03-22 19:16:31 -0500

    add page 32 turkey commandline project

---
+++ ObjectiveC_BNRG/Turkey/Turkey/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Turkey
+//
+//  Created by Ethan Peacock on 3/22/24.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
