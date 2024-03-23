b69befc96bfe352e29125cc17ea894d9b38f44db
ethan <140538808+e-d-p-9@users.noreply.github.com>
2024-03-22 19:28:20 -0500

    add calculation for cooking time and logging weight and time to user page 34

--- ObjectiveC_BNRG/Turkey/Turkey/main.c
+++ ObjectiveC_BNRG/Turkey/Turkey/main.c
@@ -8,7 +8,26 @@
 #include <stdio.h>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    // Declare the variable weight of type float
+    float weight;
+    
+    // Store a number in that variable
+    weight = 14.2;
+    
+    // log the weight to the user
+    printf("Weight of turkey is %f pounds.\n", weight);
+    
+    // Declare another variable of type float
+    float cookingTime;
+    
+    // Calculate the cooking time and store it in the variable
+    // formula was 15 minutes plus 15 minutes per pound
+    // in this case '*' means multiply
+    cookingTime = 15 + 15 * weight;
+    
+    // log cooking time to the user
+    printf("cooking time is %f minutes.\n", cookingTime);
+    
+    // end the function and indicate success
     return 0;
 }
